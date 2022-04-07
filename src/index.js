2cd56e0cf04673503bad849e389bb5bb1c2d2c5d
marcellevargas <marcelle.vargas96@gmail.com>
2022-04-07 19:38:47 -0300

    Update repo

--- src/index.js
+++ src/index.js
@@ -1,28 +1,50 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import SeasonDisplay from "./SeasonDisplay";
+import "semantic-ui-css/semantic.min.css";
+import Spinner from "./Spinner";
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = { lat: null, errorMessage: "" };
+  }
 
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        this.setState({ lat: position.coords.latitude });
-      },
-      (err) => {
-        this.setState({ errorMessage: err.message });
-      }
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ errorMessage: err.message })
+    );
+  }
+
+  renderContent() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return (
+        <div className="border red">
+          <div>Error: {this.state.errorMessage}</div>;
+        </div>
+      );
+    }
+    if (!this.state.errorMessage && this.state.lat) {
+      return (
+        <div className="border red">
+          <SeasonDisplay lat={this.state.lat} />;
+        </div>
+      );
+    }
+
+    return (
+      <div className="border red">
+        <SeasonDisplay lat={this.state.lat} />;
+      </div>
     );
   }
 
   render() {
-    if (this.state.errorMessage && !this.state.lat) {
-      return <div>Error: {this.state.errorMessage}</div>;
-    } 
-    if (!this.state.errorMessage && this.state.lat) {
-      return <div>Latitude: {this.state.lat}</div>;
-    }
+    return (
+      <div className="border red">
+        {this.renderContent()}
+      </div>
+    );
   }
 }
 
